cc11e0beb9fde945de906b3fc17c3f08f2753aad
Your Name <you@example.com>
2022-09-01 14:21:13 +0600

    project setup done

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1 className="text-4xl">
+        Hello world!
+      </h1>
+      <button className="btn btn-primary">Button</button>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#DE0404",
+          secondary: "#0FCEEB",
+          accent: "#3A4256",
+          neutral: "#004DDA",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
